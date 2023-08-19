8f3e710ac78c5e683ee36f8f2bd1a5b9ff69b7db
katherinemoore <katherinemoore789@gmail.com>
2023-08-18 18:55:09 -0700

    Add photo when game version is selected on index.html; additional formatting

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ DATE: 08/16/2023
 FINAL PROJECT
 */
 
+//Global variables
+let totalPokemon = 150;
+let count;
+
 //Close Button/Display button show/hide for About Section on index.html
 $(document).ready(function(){
     $('#show-about').hide(); //Hides the reload button upon page load
@@ -21,4 +25,19 @@ $(document).ready(function(){
         $('#about').show();
     })
     
+});
+
+//When Game Version is selected, various data is updated on page
+document.querySelector('#game-version').addEventListener('change', (e) => {
+    e.preventDefault();
+    let version = e.target.value;
+
+    //Adds an image of the game version to the document
+    let imgDiv = document.querySelector('#game-photo');
+    let imgURL = `images/game-versions/${version}.png`;
+    let altText = `The box for the game Pokemon ${version}.`
+    
+    imgDiv.innerHTML = ""; //clear possible existing photo
+    imgDiv.innerHTML = `<img src="${imgURL}" alt="${altText}">`; //Add in new photo to doc
+
 });
