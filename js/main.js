3c94a55b62b6d84cdd6410386131f538cd083e44
katherinemoore <katherinemoore789@gmail.com>
2023-08-18 17:35:39 -0700

    Created a show/hide button for about section

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ MEID: KAT2341012
 DATE: 08/16/2023
 FINAL PROJECT
 */
+
+//Close Button/Display button show/hide for About Section on index.html
+$(document).ready(function(){
+    $('#show-about').hide(); //Hides the reload button upon page load
+    
+    $('#close-about').click((e) => {
+        e.preventDefault();
+    $('#about').hide();
+    $('#show-about').show();
+    });
+
+    $('#open-about').click((e) => {
+        e.preventDefault();
+        $('#show-about').hide();
+        $('#about').show();
+    })
+    
+});
